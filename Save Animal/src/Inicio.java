7124542e7ee1f8740ad74d17e207d49abd9cfa3e
AlanAds03 <alan.ads03@gmail.com>
2015-12-13 00:54:48 -0200

    removendo referencias

--- Save Animal/src/Inicio.java
+++ Save Animal/src/Inicio.java
@@ -1,28 +1,15 @@
 import java.awt.Color;
-import java.awt.ComponentOrientation;
 import java.awt.Font;
-import java.awt.MenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.text.AttributeSet.ColorAttribute;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JColorChooser;
-
 
 public class Inicio {
 
