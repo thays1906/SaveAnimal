bfd7ab8001559d00322d52352572a0d62542c3e4
thays1906 <thayssilva1906@gmail.com>
2015-12-13 22:40:12 -0200

    menu adicionados

--- Save Animal/src/Inicio.java
+++ Save Animal/src/Inicio.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,6 +20,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTable;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
@@ -30,6 +32,8 @@ public class Inicio {
 	static Color azul = new Color(0, 191, 255);
 
 	private static JMenuItem submenu_amigos;
+
+	
 
 	public static void main(String[] args) {
 		//KKK
@@ -45,24 +49,23 @@ public class Inicio {
 
 		form.setLayout(null);
 		form.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		JLabel lblBack = new JLabel(new ImageIcon("images/imgback.fw.png"));
-		lblBack.setBounds(50, 50, 100, 100);
-		form.add(lblBack);
+		
 		JMenuBar bar= new JMenuBar();
 		bar.setBackground(azul);
 		bar.setBorder(new BevelBorder(BevelBorder.LOWERED));
-		JMenu menu1= new JMenu("                 Cadastro              ");
+		JMenu menu1= new JMenu("               Cadastro                      ");
+		menu1.setBounds(100, 100, 0, 0);
 		menu1.setBackground(azul);
 		menu1.setForeground(Color.WHITE);
 		menu1.setFont(new Font("Sans Serif", Font.BOLD, 21));
 		
-		JMenuItem submenu_cliente = new JMenuItem("   Cliente              ");
-		
+		JMenuItem submenu_cliente = new JMenuItem("     Cliente                            ");
+		 
 		ImageIcon imgcliente= new ImageIcon("images/Login Manager.png");
 		
 		submenu_cliente.setIcon(imgcliente);
 		submenu_cliente.setForeground(Color.WHITE);
-	    submenu_cliente.setFont(new Font("" ,Font.BOLD , 17));
+	    submenu_cliente.setFont(new Font("" ,Font.BOLD , 20));
 	    submenu_cliente.setBackground(azul);
 	   
 		
@@ -82,9 +85,19 @@ public class Inicio {
 		
 		menu1.add(submenu_cliente);
 		
-		JMenuItem menu_amigos = new JMenuItem("   Amigos  ");
-	    menu_amigos.setForeground(Color.WHITE);
-	    menu_amigos.setFont(new Font("" ,Font.BOLD , 17));
+		
+		ImageIcon imgamigos = new ImageIcon("images/caoegatofw.fw.png");
+		
+		
+		JMenuItem menu_amigos = new JMenuItem("     Amigos                         ");
+		
+		Image img1 = imgamigos.getImage();
+		Image newimg = img1.getScaledInstance(40, 35, java.awt.Image.SCALE_SMOOTH);
+		
+		
+		menu_amigos.setIcon(new ImageIcon(newimg));
+		menu_amigos.setForeground(Color.WHITE);
+	    menu_amigos.setFont(new Font("Serif Bold" ,Font.BOLD, 20));
 	    menu_amigos.setBackground(azul);
 	    menu_amigos.addActionListener(new ActionListener() {
 			
@@ -106,12 +119,13 @@ public class Inicio {
 		
 	
 		
-		JMenu menu3 = new JMenu("       Adoção        ");	
+		JMenu menu3 = new JMenu("                 Adoção                   ");	
 		menu3.setFont(new Font("Helvetica", Font.BOLD, 21));
 		menu3.setForeground(Color.white);
 		bar.add(menu3);
 		
-		JMenuItem submenu_adotar = new JMenuItem("  Realizar Adoção  ");
+		
+		JMenuItem submenu_adotar = new JMenuItem("  Realizar Adoção                 ");
 		submenu_adotar.setForeground(Color.WHITE);
 	    submenu_adotar.setFont(new Font("" ,Font.BOLD , 17));
 	    submenu_adotar.setBackground(azul);
@@ -120,7 +134,7 @@ public class Inicio {
 	    submenu_adotar.setIcon(imggato);
 		menu3.add(submenu_adotar);
 		
-		JMenuItem submenu_relatorio = new JMenuItem("  Relatório de Adoções");
+		JMenuItem submenu_relatorio = new JMenuItem("  Relatório de Adoções              ");
 		submenu_relatorio.setForeground(Color.WHITE);
 		submenu_relatorio.setFont(new Font("" ,Font.BOLD , 17));
 		submenu_relatorio.setBackground(azul);
@@ -128,6 +142,18 @@ public class Inicio {
 		ImageIcon imgrelatorio = new ImageIcon("images/relatorio.fw.png");
 		submenu_relatorio.setIcon(imgrelatorio);
 		menu3.add(submenu_relatorio);
+		
+		JMenu menu4 = new JMenu("           Sobre                         ");
+		menu4.setFont(new Font("Helvetica", Font.BOLD, 21));
+		menu4.setForeground(Color.white);
+		bar.add(menu4);
+		
+		JMenu menu5 = new JMenu(" Logout                           ");
+		ImageIcon sair = new ImageIcon("images/logout.png");
+		menu5.setIcon(sair);
+		menu5.setFont(new Font("Helvetica", Font.BOLD, 21));
+		menu5.setForeground(Color.white);
+		bar.add(menu5);
 		
 		form.setJMenuBar(bar);
 		form.setSize(800, 800);
@@ -138,59 +164,7 @@ public class Inicio {
 		
 		imglogo.setBounds(100, 100, 400, 400);
 		
-		//form.add(imglogo);
-		String[] colunas = new String[]{"Nome","Idade","Sexo"};
-		String[][] dados = new String[][]{
-			{"Rodrigo","28","Masculino"},
-			{"Maria","30","Feminino"}
-		};
-		
-		
-		JTable tabela = new JTable(dados,colunas);
-		tabela.setVisible(true);
-		JScrollPane scroll = new JScrollPane();
-		scroll.setViewportView(tabela);
-		scroll.setVisible(true);
-		scroll.setBounds(20,20,200,200);
-		
-		tabela.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				
-				JOptionPane.showMessageDialog(null,tabela.getValueAt(tabela.getSelectedRow(), 0));
-				
-				
-			}
-		});
-		
-		
-		form.add(scroll);
+	
 		form.getContentPane().setBackground(Color.decode("#CAE1FF"));
 		//form.setEnabled(false);
 		
